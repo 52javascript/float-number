b9722734c142bddfe3ac57151b1e6243099f9d35
wuxx <wuxxself@163.com>
2020-03-16 11:14:51 +0800

    修改为原型方法

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const {
 } = require('./utils/validate')
 
 const {
-    mergeOptions
+    mergeOptions,
+    mergeKey
 } = require('./utils/utils')
 
 const {
@@ -11,8 +12,7 @@ const {
 } = require('./core')
 
 function Calc (options) {
-    console.log('执行力了构造函数')
-    this.prototype._base = this
+    console.log('执行了构造函数')
     this._options = mergeOptions(options) // 合并配置项
 }
 
@@ -25,7 +25,6 @@ function Calc (options) {
      */
     
 Calc.prototype.add = function (a, b, options = {}) {
-    // console.log(this._base)
     this._options = mergeOptions(options)
     const _arguments = mergeArgumentsIsNumber([...arguments])
     return _add.call(this, _arguments[0], _arguments[1])
@@ -44,4 +43,5 @@ Calc.prototype.del = function (a, b, options = {}) {
     // 减法相当于加上b的负数
     return _add.call(this, _arguments[0], -(_arguments[1]))
 }
-module.exports =  Calc
+exports.Calc =  Calc
+exports.add = Calc.prototype.add
--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-const Calc = require ('../src/index')
+const {Calc} = require ('../src/index')
+const {add} = require ('../src/index')
 
-// const obj = new Calc({
-//     fixed: 2
-// })
-// let a = obj.add(0.1, '2.03', {fixed: 4})
-let b = Calc.prototype.add(0.1, 0.2, {returnString: true})
-// console.log(a)
+const obj = new Calc({
+    fixed: 2
+})
+let a = obj.add(0.1, '2.03', {fixed: 4})
+let b = add(0.1, 0.2, {returnString: true})
+console.log(a)
 console.log(b)
